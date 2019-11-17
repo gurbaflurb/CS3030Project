0503b75b3a76c6bfbecb5dd4e1214dfcdd98806e
Dillon Jaap <dillonjaap@gmail.com>
2019-11-17 10:01:29 -0700

    bot can send pictures

--- bot.py
+++ bot.py
@@ -1,25 +1,24 @@
-#!/usr/bin/python3
-
-import discord
 import os
-
+import random
 from dotenv import load_dotenv
 
+import discord
+from discord.ext import commands
+
+# load environment variables (relavent ones are stored in .env)
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+token     = os.getenv('DISCORD_TOKEN')
+templates = os.getenv('MEME_TEMPLATE_DIR')
 
-client = discord.Client()
+# create bot object (subclass of client object)
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-	print('We have logged in as {0.user}'.format(client))
+    print(f'{bot.user.name} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-	if(message.author == client.user):
-		return
+@bot.command(name="pic")
+async def send_pic(ctx):
+    await ctx.channel.send(file=discord.File(f'{PIC_DIR}/vapor.jpg'))
 
-	if message.content.startswith('$hello'):
-		await message.channel.send('Hello!')
-
-client.run(token)
+bot.run(token)
