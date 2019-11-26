f14297fc309e6b308b48b19ac661a4a9eae710b0
Gurbe <seachen4411@gmail.com>
2019-11-26 16:46:10 -0700

    Updated bot.py and made .gitignore

--- bot.py
+++ bot.py
@@ -2,39 +2,60 @@
 
 import discord
 import history
+from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-	print('We have logged in as {0.user}'.format(client))
+	print('We have logged in as {0.user}'.format(bot))
 
+@bot.command(name='echo')
+async def echo(ctx, *args):
+	await ctx.send(' '.join(args))
 
-@client.event
-async def on_message(message):
-	if(message.author == client.user):
-		return
+@echo.error
+async def echo_error(ctx, error):
+	await ctx.send("You didn't give me anything to echo ಥ_ಥ")
 
-	if message.content.startswith('!hello'):
-		await message.channel.send('Hello!')
-	elif message.content.startswith('!echo'):
-		await message.channel.send(message.content[5:])
-	elif(message.content.startswith('!history')):
-		pastChat = await history.getHistory(message)
-		if(pastChat == None):
-			await message.channel.send("Invalid number ¯\\_(ツ)_/¯")
+@bot.command(name='history')
+async def getHistory(ctx, arg: int):
+	history = await ctx.history(limit=arg).flatten()
+	print(type(history.content))
+	returnHistory = []
+	for chatMsg in history:
+		if('!history' in chatMsg.content or chatMsg.content == ''):
+			num = num+1
+			continue
 		else:
-			for msg in pastChat:
-				await message.channel.send(msg)
-	elif(message.content.startswith('!spongebob')):
-		spongebobText = await history.spongeBobText(message)
-		if spongebobText == None:
-			await message.channel.send("Couldn't find text ¯\\_(ツ)_/¯")
-		else:
-			await message.channel.send(spongebobText)
-		
+			returnHistory.append(chatMsg.content)
+	await ctx.send(returnHistory)
+
+@getHistory.error
+async def history_error(ctx, error):
+	if isinstance(error, commands.ArgumentParsingError):
+		await ctx.send("You didn't give me valid arguments ಥ_ಥ")
 		
 
+'''
+async def spongeBobText(message):
+	if(message.content == '!spongebob'):
+		history = await message.channel.history(limit=100).flatten()
+		for word in history:
+			if(word.content.startswith('!spongebob')):
+				continue
+			else:
+				tempText = word.content
+				break
+		returnChar = ''
+		for character in tempText:
+			randValue = random.randint(0,1)
+			if(randValue == 0):
+				returnChar = returnChar + character.upper()
+			else:
+				returnChar = returnChar+character
+		return returnChar
+'''
 print('Please enter the Authentication Token')
 token = input()
-client.run(token)
+bot.run(token)
