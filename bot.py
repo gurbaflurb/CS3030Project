e975c290f9d5eb59ac610c7dc4779259502f6156
Gurbe <seachen4411@gmail.com>
2019-11-26 17:20:44 -0700

    Modified bot.py and moved things to cogs/FunAndGames

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@
 import discord, os
 from discord.ext import commands
 from dotenv import load_dotenv
-import history
 
 # Local Files
 from cogs.Memes import Memes
@@ -29,42 +28,5 @@ async def echo(ctx, *args):
 async def echo_error(ctx, error):
 	await ctx.send("You didn't give me anything to echo ಥ_ಥ")
 
-@bot.command(name='history')
-async def getHistory(ctx, arg: int):
-	history = await ctx.history(limit=arg).flatten()
-	print(type(history.content))
-	returnHistory = []
-	for chatMsg in history:
-		if('!history' in chatMsg.content or chatMsg.content == ''):
-			num = num+1
-			continue
-		else:
-			returnHistory.append(chatMsg.content)
-	await ctx.send(returnHistory)
-
-@getHistory.error
-async def history_error(ctx, error):
-	if isinstance(error, commands.ArgumentParsingError):
-		await ctx.send("You didn't give me valid arguments ಥ_ಥ")
-		
-'''
-async def spongeBobText(message):
-	if(message.content == '!spongebob'):
-		history = await message.channel.history(limit=100).flatten()
-		for word in history:
-			if(word.content.startswith('!spongebob')):
-				continue
-			else:
-				tempText = word.content
-				break
-		returnChar = ''
-		for character in tempText:
-			randValue = random.randint(0,1)
-			if(randValue == 0):
-				returnChar = returnChar + character.upper()
-			else:
-				returnChar = returnChar+character
-		return returnChar
-'''
 bot.run(token)
 
--- cogs/FunAndGames.py
+++ cogs/FunAndGames.py
@@ -26,3 +26,44 @@ class FunAndGames(commands.Cog):
             await ctx.guild.kick(ctx.author)
         else:
             await ctx.channel.send("*click*")
+    
+    @commands.command(name='history')
+    async def getHistory(self, ctx, arg: int):
+        history = await ctx.history(limit=arg).flatten()
+        print(type(history.content))
+        returnHistory = []
+        for chatMsg in history:
+            if('!history' in chatMsg.content or chatMsg.content == ''):
+                num = num+1
+                continue
+            else:
+                returnHistory.append(chatMsg.content)
+        await ctx.send(returnHistory)
+
+    @getHistory.error
+    async def history_error(self, ctx, error):
+        if isinstance(error, commands.ArgumentParsingError):
+            await ctx.send("You didn't give me valid arguments ಥ_ಥ")
+        else:
+            await ctx.send("An error has occured oof")
+    
+    '''
+    @commands.command(name='spongebob')
+    async def spongeBobText(self, ctx, arg):
+        if(message.content == '!spongebob'):
+            history = await message.channel.history(limit=100).flatten()
+            for word in history:
+                if(word.content.startswith('!spongebob')):
+                    continue
+                else:
+                    tempText = word.content
+                    break
+            returnChar = ''
+            for character in tempText:
+                randValue = random.randint(0,1)
+                if(randValue == 0):
+                    returnChar = returnChar + character.upper()
+                else:
+                    returnChar = returnChar+character
+            return returnChar
+    '''
