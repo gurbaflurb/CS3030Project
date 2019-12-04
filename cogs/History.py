9715093707d416a2a86a6dc88af61c8f0bf0aaf5
Dillon Jaap <dillonjaap@gmail.com>
2019-12-04 12:26:04 -0700

    add/del history per channel or all channels

--- cogs/History.py
+++ cogs/History.py
@@ -13,7 +13,7 @@ class History(commands.Cog):
         self.bot = bot
         self.db = shelve.open('history.db')
 
-    @commands.command(name='list-hist')
+    @commands.command(name='list-all')
     @commands.has_any_role('mod', '@mod')
     async def listHistory(self, ctx):
         for key in self.db.keys():
@@ -24,35 +24,65 @@ class History(commands.Cog):
                 print(f"\t{channel_name}: {channel}")
 
 
+    @commands.command(name='list-channels')
+    @commands.has_any_role('mod', '@mod')
+    async def listChannels(self, ctx):
+        for channel in self.db[str(ctx.guild.id)]:
+            channel_name = self.bot.get_channel(id=int(channel))
+            print(f"{channel_name}")
+            await ctx.send(f"{channel_name}")
+
+
     @commands.command(name='del-hist')
     @commands.has_any_role('mod', '@mod')
-    async def delHistory(self, ctx):
-        for key in self.db.keys():
-            del self.db[key]
+    async def delHistory(self, ctx, *args):
+        channels = await self.filter_channels(ctx, args)
+        srv_id = str(ctx.guild.id)
+        server = self.db[srv_id]
+
+        # if 0 args, delete everything
+        if len(args) == 0:
+            print(f"deleting saved history of all channels")
+            await ctx.send(f"deleting saved history of all channels")
+            self.db[srv_id] = {}
+            return
+
+        for channel in channels:
+            print(f"deleting saved history of {channel}")
+            await ctx.send(f"deleting saved history of {channel}")
+            del server[channel.id]
+        self.db[srv_id] = server
 
 
     @commands.command(name='add-hist')
     @commands.has_any_role('mod', '@mod')
-    async def getHistory(self, ctx):
-        print(f"Grabbing messages from {ctx.channel.name} this could take a while...")
-        await ctx.send(f"Grabbing messages from {ctx.channel.name} this could take a while...")
+    async def getHistory(self, ctx, *args):
+        channels = await self.filter_channels(ctx, args)
 
-        history = await ctx.history(limit=None, oldest_first=True).flatten()
-        del history[0] # ignore message that called this command
+        # if server has not been saved in the db initialize it
+        srv_id   = str(ctx.guild.id)
+        if srv_id not in self.db: 
+            self.db[srv_id] = {}
 
-        server  = str(ctx.guild.id)
-        channel = str(ctx.channel.id)
-        filtered = await self.filter_messages(history)
+        for arg in args:
+            if arg not in [i.name() for i in channels]:
+                print(f"no channel named {arg}")
+                await ctx.send(f"no channel named {arg}")
 
-        
-        if server not in self.db:
-            self.db[server] = {}
-        srv_dict = self.db[server]
-        srv_dict[channel] = filtered
-        self.db[server] = srv_dict
+        for channel in channels:
+            print(f"Grabbing messages from {channel} this could take a while...")
+            await ctx.send(f"Grabbing messages from {channel} this could take a while...")
 
-        print(f"Grabbed {len(filtered)} messages from {ctx.channel.name}")
-        await ctx.send(f"Grabbed {len(filtered)} messages from {ctx.channel.name}")
+            history  = await channel.history(limit=None, oldest_first=True).flatten()
+            filtered = await self.filter_messages(history)
+
+            # store channel history in server entry
+            srv_dict = self.db[srv_id]
+            srv_dict[channel.id] = filtered
+            self.db[srv_id] = srv_dict
+
+            print(f"Grabbed {len(filtered)} messages from {channel}")
+            await ctx.send(f"Grabbed {len(filtered)} messages from {channel}")
 
     @getHistory.error
     async def history_error(self, ctx, error):
@@ -61,6 +91,22 @@ class History(commands.Cog):
         else:
             await ctx.send(f"An  error has occured oof:\n !f{error}")
 
+
+    async def filter_channels(self, ctx, *args):
+        """ 
+        Given a list of channels, returns the ones that actually exist
+        If no arguments are given, return all channels
+        """
+        channels = ctx.guild.text_channels
+        new_channels = []
+        if len(args) == 1: # if no channels specified grab them all
+            return channels
+
+        for channel in channels:
+            if channel.name in args:
+                new_channels.append(channel)
+        return new_channels
+        
 
     async def filter_messages(self, messages: list):
         filtered = []
