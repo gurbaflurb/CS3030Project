7b2d2692453c3fa6d7438d80e0527a4674b4f185
Gurbe <seachen4411@gmail.com>
2019-12-10 15:30:43 -0700

    Moved word filter to cogs.admin, fixed bot not recognizing commands when word filter is active

--- bot.py
+++ bot.py
@@ -19,17 +19,11 @@ bot.add_cog(Memes(bot))
 bot.add_cog(FunAndGames(bot))
 bot.add_cog(History(bot))
 bot.add_cog(admin(bot))
+bot.add_listener(admin.word_filter)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
-
-@bot.event
-async def on_message(message):
-    banned_words = ['fuck', 'shit', 'asshole','kys',]# List of banned words
-    for word in banned_words:
-        if word in message.content:
-            await message.delete()
 
 bot.run(token)
 
--- cogs/admin.py
+++ cogs/admin.py
@@ -19,3 +19,9 @@ class admin(commands.Cog):
     @commands.command(name='ban', hidden=True)
     async def _ban(self, ctx, *, arg):
         pass
+
+    async def word_filter(message):
+        banned_words = ['fuck', 'shit', 'asshole','kys',]# List of banned words
+        for word in banned_words:
+            if word in message.content:
+                await message.delete()
